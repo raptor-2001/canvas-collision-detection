9d248f0aa15b484133a632e6bc5221b7726722de
raptor-2001 <pratham007yadav@gmail.com>
2022-11-08 22:57:18 +0530

    collsion detection of two circles

---
+++ canvas.js
@@ -0,0 +1,94 @@
+
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+}
+
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+})
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth
+  canvas.height = innerHeight
+
+  init()
+})
+
+// Utility functions
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+// Get the distance
+function getDistance(x1, y1, x2, y2){
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDistance,2) + Math.pow(yDistance,2));
+ 
+}
+// Objects
+class Circle {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.closePath()
+  }
+
+  update() {
+    this.draw()
+  }
+}
+
+// Implementation
+let circle1;
+let circle2;
+
+function init() {
+  circle1 = new Circle(window.innerWidth/2,window.innerHeight/2,100,'black');
+  circle2 = new Circle(undefined,undefined,30,'red'); 
+}
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  circle2.x = mouse.x;
+  circle2.y = mouse.y;
+  let circleDiff = getDistance(circle1.x,circle1.y,circle2.x,circle2.y);
+  
+  if(circleDiff <= circle1.radius + circle2.radius){
+    circle1.color = 'red';
+  }else{
+    circle1.color = 'black';
+  }
+  circle2.update();
+  circle1.update();
+}
+init()
+animate()
